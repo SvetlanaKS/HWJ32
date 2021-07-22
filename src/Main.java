f4b620c63229c2d291c24d12b48a9857856caf92
Svet21 <chuyak.svetlana@gmail.com>
2021-07-22 12:48:57 +0500

    Первый коммит

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 100;
+        int refill = 1000;
+        int total = account + refill;
+        if (refill > 1000) {
+            System.out.println(total = account + refill + refill / 100);
+        } else {
+            System.out.println(total = account + refill);
+        }
+        
+    }
+}
